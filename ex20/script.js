ddc7a83784b9b8c49b83134547ebb33fadaf0cb3
Renato Matos <108771359+SonRenato1506@users.noreply.github.com>
2023-08-07 21:07:05 -0300

    Update script.js

--- ex20/script.js
+++ ex20/script.js
@@ -1,13 +1,71 @@
 function vai() {
+
     p = document.querySelector("p")
     x = 1
     y = 0
     c = 1
+    s = 0
+
+
     do {
-        p.innerHTML += `${x}, `
+        s = 1
+        primo = 0
+
+        s = 1;
+        primo = 0;
+
+        do {
+            if (x === 1) {
+                primo = 0;
+            } else {
+                if (x % s == 0) {
+                    primo++;
+                }
+            }
+            s++;
+        } while (s < x );
+
+        if (primo <= 2 && primo > 0) {
+            p.innerHTML += `<span style="color:green">${x}</span>, `;
+        }
+        else {
+
+            if (x % 2 == 0) {
+                p.innerHTML += `<span style="color:blue">${x}, <span/> `
+            } else {
+                p.innerHTML += `<span style="color:red">${x}, <span/> `
+            }
+        }
+
         c++
-        y = x +y
-        p.innerHTML += `${y}, `
+        y = x + y
+
+        s = 1;
+        primo = 0;
+
+        do {
+            if (y === 1) {
+                primo = 0;
+            } else {
+                if (y % s == 0) {
+                    primo++;
+                }
+            }
+            s++;
+        } while (s < y );
+
+        if (primo <= 2 && primo > 0) {
+            p.innerHTML += `<span style="color:green">${y}</span>, `;
+        }
+        else {
+
+        if (y % 2 == 0) {
+            p.innerHTML += `<span style="color:blue">${y}, <span/> `
+        } else {
+            p.innerHTML += `<span style="color:red">${y}, <span/> `
+        }
+    }
+
         c++
         x = x + y
 
